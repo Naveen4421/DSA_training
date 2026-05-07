120564800954dbfffe05e5c9a4a7c1f918eaad99
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-05-07 14:29:13 +0530

    Add clone linked list with random pointer functionality
    Implement a function to clone a linked list with random pointers.

---
+++ clone_linked_list.py
@@ -0,0 +1,39 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        if not head:
+            return None
+        c=head
+        while c:
+            new=Node(c.val)
+            new.next=c.next
+            c.next=new
+            c=c.next.next
+        c=head
+        while c:
+            if c.random:
+                c.next.random=c.random.next
+            c=c.next.next
+            
+        clone=head.next
+        c1=clone
+        c=head
+        while c:
+            c.next = c1.next          
+            c = c.next                
+            if c1.next:               
+                c1.next = c1.next.next
+            c1 = c1.next              
+        return clone
